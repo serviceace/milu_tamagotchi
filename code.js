10279377c1aef445e2dd50894a848dc4bb9c2cf0
serviceace <the.serviceace@gmail.com>
2024-03-26 05:47:08 -0300

    Agregando nuevas imagenes para Milu

--- code.js
+++ code.js
@@ -1,5 +1,9 @@
 const milu_display = document.querySelector('#milu_display');
 const pet_image = document.querySelector('#pet_image');
+const stats_menu = document.querySelector('#stats_menu');
+const pet_hungry = document.querySelector('#pet_hungry');
+const pet_joy = document.querySelector('#pet_joy');
+const pet_level = document.querySelector('#pet_level');
 const btn_eat = document.querySelector('#btn_eat');
 const btn_play = document.querySelector('#btn_play');
 const btn_stats = document.querySelector('#btn_stats');
@@ -10,50 +14,62 @@ const pet = {
 	name: "Milu",
 	hungry: 10,
 	joy: 0,
-	level: 0,
+	level: 1,
+	showingStats: false,
 }; 
 
 // FUNCTIONS			--------------------------------------------------
 const change_to_standby = () => {
-	milu_display.innerHTML = `
-	<img  id="pet_image" src="./images/milu_tikita.png">`;
-    console.log('Switch Milu to standby animation');
+	pet_image.style.visibility = 'visible';
+	stats_menu.style.visibility = 'hidden';
+	remove_animation();
+	pet_image.classList.add('animation_pet_standby');
+	pet_image.src = './images/milu_tikita.png'
+	
+
+	console.log('Switch Milu to standby animation');
 };
+
+const remove_animation = () => {
+	pet_image.classList.remove('animation_pet_standby');
+	pet_image.classList.remove('animation_pet_eating');
+	pet_image.classList.remove('animation_pet_playing');
+}
 
 const milu_eat = () => {
-	milu_display.innerHTML = `
-	<img  id="pet_image" src="./images/milu_comiendo.png">`;
-	console.log('Milu Hungry --');
-
+	pet_image.src = './images/milu_comiendo.png';
+	remove_animation();
+	pet_image.classList.add('animation_pet_eating');
+	pet.hungry--;
 	setTimeout(change_to_standby, 2000);
+
+	console.log('Milu Hungry --');
 };
 
-
 const milu_plays = () => {
-	milu_display.innerHTML = `
-	<img  id="pet_image" src="./images/milu_jugando.png">`;
-	console.log('Milu Joy++');
+	pet_image.src = './images/milu_jugando.png'
+	remove_animation();
+	pet_image.classList.add('animation_pet_playing');
 	pet.joy++;
-
-		if (pet.joy > 10) {
-			pet.joy = 10;
-		};
-
 	setTimeout(change_to_standby, 2000);
+
+	console.log('Milu Joy++');
 };
 
 const milu_status = () => {
 	console.log('Showing stats points');
+	remove_animation();
 	showing_stats();	
 };
 
 const showing_stats = () => {
-	milu_display.innerHTML = `
-	<div class='stats_menu'>
-		<p>Hambre: ${pet.hungry}</p>
-		<p>Felicidad: ${pet.joy}</p>
-		<p>Nivel : ${pet.level}</p>
-	</div>`;
+	pet_image.style.visibility = 'hidden';
+	stats_menu.style.visibility = 'visible';
+
+	pet_hungry.textContent = pet.hungry;
+	pet_joy.textContent = pet.joy;
+	pet_level.textContent = pet.level;
+
 
 	setTimeout(() => {
 	    change_to_standby();
@@ -62,19 +78,25 @@ const showing_stats = () => {
 
 const cheking_stats = () => {
 	console.log('Cheking stats for up level');
-		if (pet.hungry === 0 && pet.joy === 10) {
+		if (pet.hungry === 0 && pet.joy === 10 ) {
+			console.log('Milu sube de nivel');
 			pet.level++;
 			pet.hungry = 10;
 			pet.joy = 0;
 		};
+
+		if (pet.hungry < 0) {
+			pet.hungry = 0;
+		}
+
+		if (pet.joy > 10) {
+			pet.joy = 10;
+		}
 };
-
-
 
 // BUTTONS ACTIONS		--------------------------------------------------
 btn_eat.addEventListener('click', (e) => {	
 	milu_eat();
-	pet_eat_animation();
 	cheking_stats(); 
 });
 
